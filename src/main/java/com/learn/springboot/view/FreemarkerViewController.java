8c7d7028532d94c5591eafbe637d96913e1c460e
Taycen <taizhen.tan@getech.cn>
2021-10-12 19:51:44 +0800

    springboot视图层技术整合thymeleaf案例一

--- src/main/java/com/learn/springboot/view/FreemarkerViewController.java
+++ src/main/java/com/learn/springboot/view/FreemarkerViewController.java
@@ -22,6 +22,6 @@ public class FreemarkerViewController {
         list.add(new User(1, 12, "zhangsan"));
         list.add(new User(2, 14, "mazi"));
         model.addAttribute("userList", list);
-        return "userList";
+        return "freemarker";
     }
 }
--- src/main/java/com/learn/springboot/view/JspViewController.java
+++ src/main/java/com/learn/springboot/view/JspViewController.java
@@ -17,7 +17,7 @@ public class JspViewController {
     /***
      * 处理用户请求
      * note：通过视图解析器，跳转到webapp/WEB-INF/jsp/userList.jsp页面。
-     * 【当前项目新建了templates目录,springboot会默认去找templates下，导致失效，在自己的单体项目中不会】
+     * 【当前项目新建了templates目录,springboot会默认去找templates下，导致失效，在自己的单体项目中不会。优先默认去找templates目录下找,找不到才会找识图解析器"组装"的】
      * @param model
      * @return
      */
